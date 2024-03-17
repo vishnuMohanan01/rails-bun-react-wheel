84129442f7c78b1bb4db183d1a8cd869a5afbb47
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-03-17 14:14:27 +0530

    Render react for root route

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,5 @@
+import { hydrateRoot } from "react-dom/client";
 
+import App from "./src/App";
+
+hydrateRoot(document.getElementById("root"), <App />);
---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>This is App.jsx</div>
+
+export default App;
