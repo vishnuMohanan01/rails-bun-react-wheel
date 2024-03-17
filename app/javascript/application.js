12b5df7819995d04b3d647ced0586ad89a2c2b8b
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-03-17 16:03:25 +0530

    Uses createRoot instead of hydrateRoot

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import App from "./src/App";
 
-hydrateRoot(document.getElementById("root"), <App />);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
